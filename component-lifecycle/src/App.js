b1879f9f199eeb80a016fad99cc54ae6ea1890ff
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-06-01 18:06:02 -0700

    code for Assignment

---
+++ component-lifecycle/src/App.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import './App.css';
+import axios from 'axios'
+import User from './User'
+
+class App extends React.Component {
+
+  state ={
+    userData:{},
+    followers:[]
+  }
+
+  componentDidMount(){
+    axios.get('https://api.github.com/users/schandrupatla')
+    .then((res)=>{
+    axios.get("https://api.github.com/users/schandrupatla/followers").then((followersResp) => {
+      console.log("followersResp:",followersResp.data);
+      this.setState({
+        userData:res.data,
+        followers:followersResp.data})
+      })
+    })
+    .then()
+    .catch((err)=>{
+      console.log(err);
+    })
+
+  }
+
+  render(){
+  return (
+    <div className="App">
+      {/* <header height="200" className="App-header"> */}
+          <h1>{this.state.userData.login}</h1>
+          <img border-radius = "50%" src ={this.state.userData.avatar_url} alt=""/>
+        {/* </header> */}
+        <div>
+             {
+             this.state.followers.map(follower=>{
+                return <User key={follower.id} name={follower.login} avatar ={follower.avatar_url} url={follower.html_url}/>
+            })
+            }
+        </div>   
+
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ component-lifecycle/src/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const User = (props)=>{
+    console.log("User-peops:",props)
+    const {name, avatar,url} = props;
+
+
+    return(
+        <div>
+             <h5>{name}</h5>
+             <div>
+                <img border-radius="50%" height="200" width="200" src ={avatar} alt=""/>
+             </div>
+             <a href={url}>Github_url</a>
+        </div>
+    )
+}
+export default User;
